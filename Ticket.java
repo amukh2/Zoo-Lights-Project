12fe4fe7e5f5f6211797d9e6937e315824dcf86b
Anshu Mukherjee <mukherja@wlhs.wlwv.k12.or.us>
2025-01-13 14:13:30 -0800

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -28,27 +28,42 @@ public class Ticket {
         }
     }
 
-    public static void output(Ticket num){
+   public static void output(Ticket num){
         JFrame frame = new JFrame();
+        JButton button = new JButton("hi");
+        JLabel outputID = new JLabel("ID: " + num.id);
+        JLabel outputName = new JLabel("Name: " + num.name);
+        JLabel outputAge = new JLabel("Age: " + num.age);
+        JLabel outputCost = new JLabel("Cost: " + num.cost);
+        JLabel outputAlcohol = new JLabel("Alcohol: " + num.alcohol);
+        JLabel outputTrain = new JLabel("Train: " + num.train);
+        Color c = new Color(255,244,155);
 
-        // Creating instance of JButton
-        JButton button = new JButton(" GFG WebSite Click");
 
-        JTextField outputName = new JTextField("Name: " + num.name);
+        outputName.setBounds(20,50, 300,30);
+        outputID.setBounds(20,25, 300,30);
+        outputAge.setBounds(20,75, 300,30);
+        outputCost.setBounds(20,100, 300,30);
+        outputAlcohol.setBounds(20,125, 300,30);
+        outputTrain.setBounds(20,150, 300,30);
 
-
-        // adding button in JFrame
         frame.add(button);
         frame.add(outputName);
+        frame.add(outputID);
+        frame.add(outputAge);
+        frame.add(outputCost);
+        frame.add(outputAlcohol);
+        frame.add(outputTrain);
 
-        // 400 width and 500 height
         frame.setSize(500, 250);
-
-        // using no layout managers
         frame.setLayout(null);
-
-        // making the frame visible
+        UIManager.put("Label.font", "Broadway");
+        //UIManager.put("Label.", 32 );
+        SwingUtilities.updateComponentTreeUI(frame);
+        frame.getContentPane().setBackground(c);
         frame.setVisible(true);
+
+
 
     }
 }
